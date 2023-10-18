75eb08bf9aba87adac765bd1b4d7db26b362c088
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-18 22:14:50 +0700

    Update permuted_choice.hpp

--- BlockCipher/DES/C++/des/permuted_choice.hpp
+++ BlockCipher/DES/C++/des/permuted_choice.hpp
@@ -4,7 +4,7 @@
 #include <bitset>
 #include <vector>
 
-std::bitset<56> pc1(const std::bitset<64>& key);
-std::bitset<48> pc2(const std::bitset<56>& key);
+std::bitset<56> permuted_choice_1(const std::bitset<64>& key);
+std::bitset<48> permuted_choice_2(const std::bitset<56>& key);
 
 #endif
