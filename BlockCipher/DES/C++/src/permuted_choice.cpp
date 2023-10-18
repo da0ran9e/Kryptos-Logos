080705ab78dd9b68524262b8b3d3b9f6244f5892
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-18 23:30:40 +0700

    Update permuted_choice.cpp

--- BlockCipher/DES/C++/src/permuted_choice.cpp
+++ BlockCipher/DES/C++/src/permuted_choice.cpp
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 #include "../des/permuted_choice.hpp"
 #include "../des/permutations.hpp"
-using namespace std;
 
 // Hàm hoán vị PC-1
-bitset<56> permuted_choice_1(const bitset<64>& key) {
-    bitset<56> permutedKey;
-    for (int i = 0; i < 56; i++) {
-        permutedKey[55 - i] = key[64 - PC1[i]];
+std::bitset<56> permuted_choice_1(const std::bitset<64>& key) {
+    std::bitset<56> permutedKey;
+    for (int i = 0; i < 56; ++i) {
+        permutedKey[i] = key[PC1[i] - 1];
     }
     return permutedKey;
 }
 
 // Hàm hoán vị PC-2
-bitset<48> permuted_choice_2(const bitset<56>& key) {
-    bitset<48> permutedKey;
-    for (int i = 0; i < 48; i++) {
-        permutedKey[47 - i] = key[56 - PC2[i]];
+std::bitset<48> permuted_choice_2(const std::bitset<56>& key) {
+    std::bitset<48> permutedKey;
+    for (int i = 0; i < 48; ++i) {
+        permutedKey[i] = key[PC2[i] - 1];
     }
     return permutedKey;
 }
