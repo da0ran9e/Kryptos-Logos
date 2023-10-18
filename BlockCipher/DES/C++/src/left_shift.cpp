cea6dc04c3edf9eac82e9328ae9093deb8a7c8b5
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-18 23:30:37 +0700

    Update left_shift.cpp

--- BlockCipher/DES/C++/src/left_shift.cpp
+++ BlockCipher/DES/C++/src/left_shift.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #include "../des/left_shift.hpp"
 
-std::bitset<28> left_shift(const std::bitset<24>& key, int shift) {
+std::bitset<28> left_shift(const std::bitset<28>& key, int shift) {
     std::bitset<28> data = key;
     for (int i = 0; i < 28; ++i) {
         int newPos = (i + shift) % 28;
