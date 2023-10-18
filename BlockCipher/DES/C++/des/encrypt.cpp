b979912a28b5139f52950e3401e86880f94daf4a
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-19 00:24:13 +0700

    Create encrypt.cpp

---
+++ BlockCipher/DES/C++/des/encrypt.cpp
@@ -0,0 +1,33 @@
+#include "../des/initial_permutation.hpp"
+#include "../des/key_scheduler.hpp"
+#include "../des/f_function.hpp"
+
+std::bitset<64> encryptDES(std::bitset<64> data, std::bitset<64> originalKey) {
+    data = initialPermutation(data);
+    // Left half and right half initialization
+    std::bitset<32> leftHalf, rightHalf;
+    for (int i = 0; i < 32; ++i) {
+        leftHalf[i] = data[i];
+        rightHalf[i] = data[i + 32];
+    }
+
+    // Generate 16 round keys
+    std::bitset<48> subKeys[16];
+    generate_subkeys(originalKey, subKeys);
+
+    // 16 rounds
+    for (int round = 0; round < 16; ++round) {
+        std::bitset<32> tempRight = rightHalf;
+        rightHalf = leftHalf ^ f(rightHalf, subKeys[round]);
+        leftHalf = tempRight;
+    }
+
+    // Final permutation (FP)
+    std::bitset<64> encryptedData;
+    for (int i = 0; i < 32; ++i) {
+        encryptedData[i] = leftHalf[i];
+        encryptedData[i + 32] = rightHalf[i];
+    }
+
+    return encryptedData;
+}
