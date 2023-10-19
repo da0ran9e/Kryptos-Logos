cd40bcfe66b1fc0e668a6715bd017639efd4d2ca
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-19 21:51:19 +0700

    Update main.cpp

--- BlockCipher/DES/C++/main.cpp
+++ BlockCipher/DES/C++/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "des/encrypt.hpp"
+#include "des/decrypt.hpp"
+
+using namespace std;
 
 int main() {
-    std::cout << "hello";
- 
+    bitset<64> data(0x0123456789ABCDEF); // 64-bit data
+    bitset<64> originalKey(0x0123456789ABCDEF); // 64-bit original key
+
+    // Perform DES encryption
+    bitset<64> encryptedData = encryptDES(data, originalKey);
+    bitset<64> decryptedData = decryptDES(encryptedData, originalKey);
+
+    cout << "Original data: " << hex << data.to_ullong() << endl;
+    cout << "Original key: " << hex << originalKey.to_ullong() << endl;
+    cout << "Encrypted data: " << hex << encryptedData.to_ullong() << endl;
+    cout << "Decrypted data: " << hex << decryptedData.to_ullong() << endl;
+
     return 0;
 }
