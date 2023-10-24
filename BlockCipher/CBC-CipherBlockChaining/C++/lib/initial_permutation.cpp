16425015292361d47608f6e85258fec3aeda08ee
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-24 13:51:51 +0700

    Create initial_permutation.cpp

---
+++ BlockCipher/CBC-CipherBlockChaining/C++/lib/initial_permutation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "../des/initial_permutation.hpp"
+#include "../des/permutations.hpp"
+
+// Function to perform Initial Permutation (IP)
+std::bitset<64> initialPermutation(std::bitset<64> data) {
+    std::bitset<64> permutedData;
+    for (int i = 0; i < 64; ++i) {
+        permutedData[i] = data[initialPermutationTable[i] - 1];
+    }
+    //std::cout << "IP: \t\t\t\t" << permutedData << std::endl;
+    return permutedData;
+}
