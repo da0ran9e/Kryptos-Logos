e39348d343b3c20922cd5e0fc961758720fcea19
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-24 13:51:41 +0700

    Create encrypt.cpp

---
+++ BlockCipher/CBC-CipherBlockChaining/C++/lib/encrypt.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "../des/initial_permutation.hpp"
+#include "../des/final_permutation.hpp"
+#include "../des/key_scheduler.hpp"
+#include "../des/f_function.hpp"
+#include "../des/encrypt.hpp"
+#include "../des/permutations.hpp"
+
+std::bitset<64> encryptDES(std::bitset<64> data, std::bitset<64> originalKey) {
+    data = initialPermutation(data);
+    // Left half and right half initialization
+    std::bitset<32> leftHalf, rightHalf;
+    for (int i = 0; i < 32; ++i) {
+        leftHalf[i] = data[i];
+        rightHalf[i] = data[i + 32];
+    }
+
+    // Generate 16 round keys
+    std::bitset<48> subKeys[16];
+    generate_subkeys(originalKey, subKeys);
+
+    // 16 rounds
+    for (int round = 0; round < 16; ++round) {
+        std::cout << "L: " << leftHalf << " R: " << rightHalf << " K: " << subKeys[round] << std::endl;
+
+        std::bitset<32> tempRight = rightHalf;
+        rightHalf = leftHalf ^ f(rightHalf, subKeys[round]);
+        leftHalf = tempRight;
+    }
+
+    // Final permutation (FP)
+    std::bitset<64> encryptedData;
+    for (int i = 0; i < 32; ++i) {
+        encryptedData[i] = leftHalf[i];
+        encryptedData[i + 32] = rightHalf[i];
+    }
+
+    encryptedData = finalPermutation(encryptedData);
+
+    // std::bitset<64> permutedData;
+    // for (int i = 0; i < 64; ++i) {
+    //     permutedData[i] = encryptedData[finalPermutationTable[i] - 1];
+    // }
+    
+    return encryptedData;
+}
