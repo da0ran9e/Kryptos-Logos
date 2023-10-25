5d4b641a6c6df46b7e5e8c2cfb3c2ff52f2bb79e
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-25 21:40:32 +0700

    Create keys.hpp

---
+++ BlockCipher/CBC-CipherBlockChaining/C++/cbc/keys.hpp
@@ -0,0 +1,9 @@
+#ifndef KEYS_HPP
+#define KEYS_HPP
+
+#include <bitset>
+
+// Initial Vector IV
+const std::bitset<64> initialVector(std::string("0110101101110010011110010111010001101111011011000110111101100111"));
+
+#endif
