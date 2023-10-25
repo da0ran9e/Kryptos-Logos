b04f5c121f1c9fe96e052ebb57e81a0d6b8132c3
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-25 22:59:18 +0700

    Delete key_scheduler.hpp

--- BlockCipher/CBC-CipherBlockChaining/C++/des/key_scheduler.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef KEY_SCHEDULER_HPP
-#define KEY_SCHEDULER_HPP
-
-#include <bitset>
-#include <vector>
-
-void generate_subkeys(const std::bitset<64>& key, std::bitset<48> subKeys[16]);
-
-#endif
