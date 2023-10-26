84249bcec5e45800a4376cd90e2dcf8a302928f9
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-26 07:31:31 +0700

    Update CBC.cpp

--- BlockCipher/CBC-CipherBlockChaining/C++/src/CBC.cpp
+++ BlockCipher/CBC-CipherBlockChaining/C++/src/CBC.cpp
@@ -1,7 +1,7 @@
 #include <bitset>
 #include <vector>
 #include "../cbc/CBC.hpp"
-#include "../des/encrypt.hpp"
+#include "../cbc/encrypt.hpp"
 
 
 std::vector<std::bitset<64>> encryptCBC(std::vector<std::bitset<64>> plaintext, std::bitset<64> IV, std::bitset<64> originalKey) {
