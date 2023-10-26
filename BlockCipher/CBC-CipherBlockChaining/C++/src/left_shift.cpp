94c31cad7f797f17687524ed8d554daa4a1639e5
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-26 07:31:50 +0700

    Create left_shift.cpp

---
+++ BlockCipher/CBC-CipherBlockChaining/C++/src/left_shift.cpp
@@ -0,0 +1,10 @@
+#include "../cbc/left_shift.hpp"
+
+std::bitset<28> left_shift(const std::bitset<28>& key, int shift) {
+    std::bitset<28> data = key;
+    for (int i = 0; i < 28; ++i) {
+        int newPos = (i + shift) % 28;
+        data[newPos] = key[i];
+    }
+    return data;
+}
