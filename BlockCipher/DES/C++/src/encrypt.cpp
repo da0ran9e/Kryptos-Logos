f7848d82d0fe6ed9ab7c00f40cbb1dc20297de01
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-29 17:54:24 +0700

    Update encrypt.cpp

--- BlockCipher/DES/C++/src/encrypt.cpp
+++ BlockCipher/DES/C++/src/encrypt.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "../des/initial_permutation.hpp"
 #include "../des/final_permutation.hpp"
 #include "../des/key_scheduler.hpp"
@@ -7,9 +6,8 @@
 #include "../des/permutations.hpp"
 
 std::bitset<64> encryptDES(std::bitset<64> data, std::bitset<64> originalKey) {
-    std::cout << "data: " << data;
+    //IP
     data = initialPermutation(data);
-    std::cout << "--IP-->" << data << std::endl;
     // Left half and right half initialization
     std::bitset<32> leftHalf, rightHalf;
     for (int i = 0; i < 32; ++i) {
@@ -17,34 +15,25 @@ std::bitset<64> encryptDES(std::bitset<64> data, std::bitset<64> originalKey) {
         rightHalf[i] = data[i + 32];
     }
 
-    // Generate 16 round keys
+    // Generate 16 sub keys
     std::bitset<48> subKeys[16];
     generate_subkeys(originalKey, subKeys);
 
     // 16 rounds
     for (int round = 0; round < 16; ++round) {
-        std::cout << "L: " << leftHalf << " R: " << rightHalf << " K: " << subKeys[round] << std::endl;
-
         std::bitset<32> tempRight = rightHalf;
         rightHalf = leftHalf ^ f(rightHalf, subKeys[round]);
         leftHalf = tempRight;
     }
 
-    // Final permutation (FP)
+    // Assemble 2 halves
     std::bitset<64> encryptedData;
     for (int i = 0; i < 32; ++i) {
         encryptedData[i] = leftHalf[i];
         encryptedData[i + 32] = rightHalf[i];
     }
-
-    std::cout << "data: " << encryptedData;
+    //FP
     encryptedData = finalPermutation(encryptedData);
-    std::cout << "--IP-->" << encryptedData << std::endl;
-
-    // std::bitset<64> permutedData;
-    // for (int i = 0; i < 64; ++i) {
-    //     permutedData[i] = encryptedData[finalPermutationTable[i] - 1];
-    // }
     
     return encryptedData;
 }
