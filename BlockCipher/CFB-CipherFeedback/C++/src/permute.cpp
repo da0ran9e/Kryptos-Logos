5aedad00fb39d12bb4dad775ead18982b055c3a7
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-03 10:26:35 +0700

    Create permute.cpp

---
+++ BlockCipher/CFB-CipherFeedback/C++/src/permute.cpp
@@ -0,0 +1,11 @@
+#include "../cbc/permute.hpp"
+#include "../cbc/permutations.hpp"
+
+// Function to perform permutation
+std::bitset<32> permute(std::bitset<32> data) {
+    std::bitset<32> permutedData;
+    for (int i = 0; i < 32; ++i) {
+        permutedData[i] = data[permutationTable[i] - 1];
+    }
+    return permutedData;
+}
