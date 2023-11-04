1059b5f156cfa0141ffbd587cfc1cdbb241b7361
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-04 10:50:52 +0700

    Create permuted_choice.cpp

---
+++ BlockCipher/OFB-OutputFeedback/C++/src/permuted_choice.cpp
@@ -0,0 +1,19 @@
+#include "../cbc/permuted_choice.hpp"
+#include "../cbc/permutations.hpp"
+
+// Hàm hoán vị PC-1
+std::bitset<56> permuted_choice_1(const std::bitset<64>& key) {
+    std::bitset<56> permutedKey;
+    for (int i = 0; i < 56; ++i) {
+        permutedKey[i] = key[PC1[i] - 1];
+    }
+    return permutedKey;
+}
+
+std::bitset<48> permuted_choice_2(const std::bitset<56>& key) {
+    std::bitset<48> permutedKey;
+    for (int i = 0; i < 48; ++i) {
+        permutedKey[i] = key[PC2[i] - 1];
+    }
+    return permutedKey;
+}
